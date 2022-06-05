7a9062ed55677f92895f5cb72e3c74de6ada03dc
Slava <rybco@outlook.com>
2022-06-05 16:37:19 +0300

    feat(api): add websocket endpoint, websocket connection manager

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,37 +1,79 @@
 import Navbar from "./components/Navbar";
 import MessageBox from "./components/MessageBox";
 import Main from "./components/Main";
-import { useState, createRef } from "react";
+import { useState, createRef, useEffect } from "react";
 import MessageBubble from "./components/MessageBubble";
 
 function App() {
-  const ws = new WebSocket("ws://127.0.0.1:8000/ws/test");
   const [text, changeText] = useState("");
   const [messageBubbles, addBubble] = useState([]);
+  const [ws, setWebSocket] = useState(null);
   const messagesEndRef = createRef();
-  function getRandomInt(max) {
-    return Math.floor(Math.random() * max);
-  }
 
+  useEffect(() => {
+    if (ws !== null) {
+      console.log("event");
+      ws.addEventListener("message", createVolunteerBubble);
+    }
+
+    return function () {
+      console.log("clean");
+      if (ws !== null) ws.removeEventListener("message", createVolunteerBubble);
+    };
+  }, [ws]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch("http://localhost:8000/api/threads/", {
+        method: "POST",
+      });
+
+      const { thread_id } = await response.json();
+      console.log(thread_id);
+      return thread_id;
+    }
+
+    fetchData().then((t) => {
+      setWebSocket(new WebSocket("ws://127.0.0.1:8000/ws/" + t));
+    });
+  }, []);
+
+  function createVolunteerBubble(event) {
+    console.log(event);
+    const type = "message-bubble-volunteer";
+    if (event.data) {
+      addBubble((bubbles) => [
+        <MessageBubble
+          key={bubbles.length + 1}
+          text={event.data}
+          type={type}
+          flexibleMargin={
+            bubbles.length === 0 ? 16 : bubbles[0].props.type === type ? 8 : 16
+          }
+        />,
+        ...bubbles,
+      ]);
+    }
+  }
   function scrollToBottom() {
     messagesEndRef.current.scrollIntoView({ behavior: "smooth" });
   }
+
   function createBubble() {
-    const types = ["message-bubble-user", "message-bubble-volunteer"];
-    var randomType = types[getRandomInt(2)];
+    const type = "message-bubble-user";
 
     if (text !== "") {
-      ws.send(text);
+      if (ws !== null) ws.send(text);
       addBubble((bubbles) => {
         return [
           <MessageBubble
             key={bubbles.length + 1}
             text={text}
-            type={randomType}
+            type={type}
             flexibleMargin={
               bubbles.length === 0
                 ? 16
-                : bubbles[0].props.type === randomType
+                : bubbles[0].props.type === type
                 ? 8
                 : 16
             }
