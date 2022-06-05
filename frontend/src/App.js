c67095de8586f45adbe81eafe7e88ac03b6ae73f
Slava <rybco@outlook.com>
2022-06-05 16:37:19 +0300

    Added WebSocket connection

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,46 @@
 import Navbar from "./components/Navbar";
 import MessageBox from "./components/MessageBox";
 import Main from "./components/Main";
-import { useState, createRef } from "react";
+import { useState, createRef, useEffect } from "react";
 import MessageBubble from "./components/MessageBubble";
+var ws = new WebSocket("ws://127.0.0.1:8000/ws/test");
 
 function App() {
-  const ws = new WebSocket("ws://127.0.0.1:8000/ws/test");
   const [text, changeText] = useState("");
   const [messageBubbles, addBubble] = useState([]);
   const messagesEndRef = createRef();
-  function getRandomInt(max) {
-    return Math.floor(Math.random() * max);
-  }
 
+  useEffect(() => {
+    ws = new WebSocket("ws://127.0.0.1:8000/ws/test");
+    ws.addEventListener("message", createVolunteerBubble);
+    return function () {
+      ws.removeEventListener("message", createVolunteerBubble);
+    };
+  }, []);
+
+  function createVolunteerBubble(event) {
+    console.log(event);
+    const type = "message-bubble-volunteer";
+    if (event.data) {
+      addBubble((bubbles) => [
+        <MessageBubble
+          key={bubbles.length + 1}
+          text={event.data}
+          type={type}
+          flexibleMargin={
+            bubbles.length === 0 ? 16 : bubbles[0].props.type === type ? 8 : 16
+          }
+        />,
+        ...bubbles,
+      ]);
+    }
+  }
   function scrollToBottom() {
     messagesEndRef.current.scrollIntoView({ behavior: "smooth" });
   }
+
   function createBubble() {
-    const types = ["message-bubble-user", "message-bubble-volunteer"];
-    var randomType = types[getRandomInt(2)];
+    const type = "message-bubble-user";
 
     if (text !== "") {
       ws.send(text);
@@ -27,11 +49,11 @@ function App() {
           <MessageBubble
             key={bubbles.length + 1}
             text={text}
-            type={randomType}
+            type={type}
             flexibleMargin={
               bubbles.length === 0
                 ? 16
-                : bubbles[0].props.type === randomType
+                : bubbles[0].props.type === type
                 ? 8
                 : 16
             }
