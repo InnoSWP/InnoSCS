049719fcd8042e75097c5b3f2d7d3be2abbf9ed1
macoyshev <make.oynoshev@gmail.com>
2022-06-07 00:21:22 +0300

    CORS disabled

--- backend/app/api/__init__.py
+++ backend/app/api/__init__.py
@@ -1,10 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
-from app.api.routers import volunteer, threads
+from app.api.routers import threads, volunteer
 
 
 def create_app() -> FastAPI:
     app = FastAPI()
+
+    app.add_middleware(
+        CORSMiddleware,
+        allow_credentials=True,
+        allow_orgins=['*'],
+        allow_methods=['*'],
+        allow_headers=['*'],
+    )
+
     app.include_router(volunteer.router)
     app.include_router(threads.router)
 
