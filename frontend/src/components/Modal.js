eb4b797584693d18e40da6c2f7c36b3d205b9a3d
Slava <rybco@outlook.com>
2022-06-15 20:14:57 +0300

    Documented Modal.js

--- frontend/src/components/Modal.js
+++ frontend/src/components/Modal.js
@@ -1,5 +1,10 @@
 import ReactDOM from "react-dom";
-
+/**
+ * Modal component is a wrapper for Popup menus such as {@link KebabMenu} and {@link SubmitProblemNotification}
+ * @param {{isOpen: boolean, onClose: function}} props
+ * @param {boolean} isOpen a boolean value that represents the state of the Modal Window
+ * @param {function} onClose a function that executes if the user clicks outside of the Popup Menu
+ */
 export default function Modal({ isOpen, onClose, children }) {
   if (!isOpen) return null;
   return ReactDOM.createPortal(
