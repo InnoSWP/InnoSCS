c15361af47f3a12a084a70d655d6e68b130f246c
macoyshev <make.oynoshev@gmail.com>
2022-06-15 10:56:21 +0300

    Message schema added & Support schema changed

--- backend/app/api/schemas.py
+++ backend/app/api/schemas.py
@@ -1,27 +1,49 @@
 from typing import Optional
+from enum import Enum
+from datetime import datetime
 
 from pydantic import BaseModel
 
 
+class Sender(str, Enum):
+    volunteer = 'volunteer'
+    client = 'client'
+
+
+class MessageBase(BaseModel):
+    created_at: datetime
+    content: str
+    sender: Sender
+
+
+class MessageCreate(MessageBase):
+    pass
+
+
+class Message(MessageBase):
+    pass
+
+
 class SupportBase(BaseModel):
-    questions: list[str]
+    questions: str
+    client_id: int
+    volunteer_id: Optional[int]
+    messages: Optional[list[Message]]
 
     class Config:
         orm_mode = True
 
 
 class SupportCreate(SupportBase):
-    ws_id: Optional[int] = None
+    pass
 
 
 class SupportThread(SupportBase):
-    ws_id: int
+    id: int
 
 
 class VolunteerBase(BaseModel):
     tg_id: int
-    username: str
-    thread_ws_id: Optional[int] = None
 
     class Config:
         orm_mode = True
