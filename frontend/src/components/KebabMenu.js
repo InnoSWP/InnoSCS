79b65297056f3d3b278e684f446b24f5f5bc635d
Slava <rybco@outlook.com>
2022-06-15 20:27:31 +0300

    Documented KebabMenu.js

--- frontend/src/components/KebabMenu.js
+++ frontend/src/components/KebabMenu.js
@@ -1,19 +1,26 @@
 import "./styles/kebabMenu.css";
-export default function KebabMenu(props) {
-  const options = props.optionsData.map((opt) => (
+/**
+ * KebabMenu component is a vertical dots button that toggles Popup Menu
+ * @param {{optionsData: Array.<{optionName: string, onClick: function}>, active: boolean, togglePopup: function}} props
+ * @param optionsData contains the information of the MenuOption
+ * @param {boolean} active represents the state of the KebabMenu
+ * @param {function} togglePopup opens/closes the Popup Menu
+ */
+export default function KebabMenu({ optionsData, active, togglePopup }) {
+  const options = optionsData.map((opt) => (
     <MenuOption
       key={opt.optionName}
       optionName={opt.optionName}
       onClick={() => {
         opt.onClick();
-        props.togglePopup(false);
+        togglePopup(false);
       }}
     />
   ));
   return (
     <div
-      className={props.active ? "popup-wrapper activated" : "popup-wrapper"}
-      onClick={() => props.togglePopup(false)}
+      className={active ? "popup-wrapper activated" : "popup-wrapper"}
+      onClick={() => togglePopup(false)}
     >
       <div className="kebab-menu">{options}</div>
     </div>
