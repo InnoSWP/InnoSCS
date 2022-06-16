865625933aea3d8d6fc8c16df446a3176c90a5a6
Slava <rybco@outlook.com>
2022-06-16 19:57:49 +0300

    Migrated Modal.js to TS

--- frontend/src/components/Modal.tsx
+++ frontend/src/components/Modal.tsx
@@ -1,3 +1,4 @@
+import { MouseEventHandler } from "react";
 import ReactDOM from "react-dom";
 /**
  * Modal component is a wrapper for Popup menus such as {@link KebabMenu} and {@link SubmitProblemNotification}
@@ -5,7 +6,14 @@ import ReactDOM from "react-dom";
  * @param {boolean} isOpen a boolean value that represents the state of the Modal Window
  * @param {function} onClose a function that executes if the user clicks outside of the Popup Menu
  */
-export default function Modal({ isOpen, onClose, children }) {
+
+type Props = {
+  isOpen: boolean,
+  onClose: MouseEventHandler<HTMLDivElement>,
+  children?: React.ReactNode
+}
+
+export default function Modal({ isOpen, onClose, children }: Props) {
   if (!isOpen) return null;
   return ReactDOM.createPortal(
     <div id="modal" className="modal-container" onClick={onClose}>
