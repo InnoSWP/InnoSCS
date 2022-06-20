75799f10c75e114180e4714e121a02e2f22e2a33
macoyshev <make.oynoshev@gmail.com>
2022-06-20 21:51:42 +0300

    fix: correct websocket rejection

--- backend/app/api/exceptions.py
+++ backend/app/api/exceptions.py
@@ -4,8 +4,3 @@ from fastapi.exceptions import HTTPException
 class EntityNotFound(HTTPException):
     def __init__(self, entity: str = 'entity') -> None:
         super().__init__(status_code=404, detail=f'{entity} not found')
-
-
-class FullWsRoomException(HTTPException):
-    def __init__(self) -> None:
-        super().__init__(status_code=403, detail='Room is full')
