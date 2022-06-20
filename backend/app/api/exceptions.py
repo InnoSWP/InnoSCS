951759141e8bb661ade43c6cb47c13e1e02560c6
macoyshev <make.oynoshev@gmail.com>
2022-06-20 10:35:13 +0300

    refactor(api): delete optional annotation

--- backend/app/api/exceptions.py
+++ backend/app/api/exceptions.py
@@ -1,8 +1,6 @@
-from typing import Optional
-
 from fastapi.exceptions import HTTPException
 
 
 class EntityNotFound(HTTPException):
-    def __init__(self, entity: Optional[str] = 'Entity') -> None:
+    def __init__(self, entity: str = 'entity') -> None:
         super().__init__(status_code=404, detail=f'{entity} not found')
